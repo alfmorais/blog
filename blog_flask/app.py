662c450615f67cc38198bd5a8818d832010832c7
Alfredo de Morais <alfredneto@hotmail.com>
2021-05-23 21:05:45 -0300

    Projeto concluido

--- blog_flask/app.py
+++ blog_flask/app.py
@@ -4,7 +4,7 @@ from werkzeug.exceptions import abort
 
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('D:/blog/blog/blog_flask/database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -54,3 +54,36 @@ def create():
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         'WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted'.format(post['title']))
+    return redirect(url_for('index'))
